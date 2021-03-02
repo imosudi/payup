735c215e6059ab0f2bc4a508b389fa1450ead718
Mosudi Isiaka <imosudi@gmail.com>
2021-03-02 08:47:57 +0100

    JavaScript live digital clock

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
 
+
+
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -17,6 +19,7 @@ moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 mysql = MySQL(app)
+
 
 
 from app import routes
